3054f14789f9a0423ad8cb226c84076dbbf9da37
jesus <jmlopezfabian@gmail.com>
2025-08-03 21:10:45 -0600

    Finishing home page

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ def home():
 
 @app.route("/about")
 def index():
-    return render_template("index.html")
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
